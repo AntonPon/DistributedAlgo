31db6f8eff256d1ed5e3cfff8aa59f22a0c6225c
AntonPon <ponomarchuk.anton@gmail.com>
2018-03-07 11:45:56 +0200

    added ingle-thread realization of the prefix sum

--- src/main/prefix_sum/PrefixSum.java
+++ src/main/prefix_sum/PrefixSum.java
@@ -2,8 +2,24 @@ package main.prefix_sum;
 
 public class PrefixSum {
 
+public static int[] prefixSum(int[]  array ){
+ int[] result = new int[array.length + 1];
+ result[0] = 0;
+ for (int i = 0; i < array.length; i ++){
+     result[i+1] = result[i] + array[i];
+ }
+
+ return result;
+}
+
 
     public static void main(String[] args){
-        
+    int[] numbers = new int[]{1,1,1,1,1,1,1,1,1};
+    int[] res =  prefixSum(numbers);
+
+    for (int i = 0; i < res.length; i++){
+        System.out.print(res[i]+ " ");
     }
+}
+
 }
